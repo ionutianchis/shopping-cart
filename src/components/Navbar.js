7e420edb9d0aff5dd4b10a87820dd35d79cc1aa3
Jonthejon10 <84473793+Jonthejon10@users.noreply.github.com>
2021-10-07 08:35:11 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,15 +52,15 @@ const Navbar = ({ cart, setCart, swords }) => {
             <h1 onClick={() => history.push('/')}>Blackbeard's forge</h1>
             
             <ul className='nav-list'>
-                <Link to='/' className='nav-link styled-link'>
+                <Link to='/shopping-cart/' className='nav-link styled-link'>
                     <li>Home</li>
                 </Link>
                     
-                <Link to='/shop' className='nav-link styled-link'>
+                <Link to='/shopping-cart/shop' className='nav-link styled-link'>
                     <li>Shop</li>
                 </Link>
                     
-                <Link to='/contact' className='nav-link styled-link'>
+                <Link to='/shopping-cart/contact' className='nav-link styled-link'>
                     <li>Contact</li>
                 </Link>
 
